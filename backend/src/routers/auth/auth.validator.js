b44ad1197f946d31ca537bd67d3a62816903a349
Yaser Hajian <mohammadyaser1379@gmail.com>
2023-06-29 10:10:27 +0330

    auth is based on username now

--- backend/src/routers/auth/auth.validator.js
+++ backend/src/routers/auth/auth.validator.js
@@ -1,18 +1,11 @@
-const expressValidator = require('express-validator');
+const expressValidator = require("express-validator");
 const check = expressValidator.check;
 
-module.exports = new class{
-
-
-  loginValidator(){
+module.exports = new (class {
+  loginValidator() {
     return [
-      check('email')
-        .isEmail()
-        .withMessage('email is invalid'),
-      check('password')
-        .not()
-        .isEmpty()
-        .withMessage('password cant be empty'),
-    ]
+      check("username").not().isEmpty().withMessage("username is invalid"),
+      check("password").not().isEmpty().withMessage("password cant be empty"),
+    ];
   }
-}
+})();
