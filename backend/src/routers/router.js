71dabc8d36552119b2e8c59f90b00f49a906775c
Yaser Hajian <mohammadyaser1379@gmail.com>
2023-06-30 20:35:09 +0330

    separate the login and signout route from main file.

--- backend/src/routers/router.js
+++ backend/src/routers/router.js
@@ -1,60 +1,25 @@
 const express = require("express");
-const router = express.Router();
 const authGuard = require("./../middleware/auth.guard");
 const roleGuard = require("./../middleware/role.guard");
-const redisAuthService = require("../redis/index");
-const User = require("../models/user");
 const getMeController = require("./term/controller/getMeController");
 const getProfessorsController = require("./term/controller/getProfessorsConroller");
 const getStudentsController = require("./term/controller/getStudentsConroller");
 const acceptOrRejectRegistrationController = require("./term/controller/acceptOrRejectRegistrationController");
-const adminRouter = require("./admin");
 const getTermsController = require("./term/controller/getTermsController");
+const getCoursesController = require("./course/controllers/getCoursesController");
+const loginController = require("../utils/login");
+const signoutController = require("../utils/signout");
+const adminRouter = require("./admin");
 const termRouter = require("./term");
 const courseRouter = require("./course");
-const getCoursesController = require("./course/controllers/getCoursesController");
+
+const router = express.Router();
 
 router.use("/", authGuard, roleGuard());
 
-router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const user = await User.findOne({ username, password });
-    if (user) {
-      const token = redisAuthService.signJWT({ id: username });
-      const refreshToken = redisAuthService.createRefreshToken();
-      await redisAuthService.addToken(username, refreshToken, token);
-      return res
-        .status(200)
-        .json({ error: false, data: { token: refreshToken } });
-    } else {
-      return res
-        .status(401)
-        .json({ error: true, message: "Unauthorized." })
-        .end();
-    }
-  } catch (err) {
-    console.log(err);
-    return res
-      .status(500)
-      .json({ error: true, message: "InternalError" })
-      .end();
-  }
-});
+router.post("/login", loginController);
 
-router.get("/signout", async (req, res) => {
-  const refreshToken = req.headers.authorization;
-  const id = req.user.username;
-  try {
-    await redisAuthService.clearToken(id, refreshToken);
-    return res.status(200).json({ error: false }).end();
-  } catch (err) {
-    return res
-      .status(500)
-      .json({ error: true, message: "InternalError" })
-      .end();
-  }
-});
+router.get("/signout", signoutController);
 
 router.get("/me", getMeController);
 router.get("/terms", getTermsController);
---
+++ backend/src/utils/login.js
@@ -0,0 +1,30 @@
+const User = require("../models/user");
+const redisAuthService = require("../redis/index");
+
+const loginController = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username, password });
+    if (user) {
+      const token = redisAuthService.signJWT({ id: username });
+      const refreshToken = redisAuthService.createRefreshToken();
+      await redisAuthService.addToken(username, refreshToken, token);
+      return res
+        .status(200)
+        .json({ error: false, data: { token: refreshToken } });
+    } else {
+      return res
+        .status(401)
+        .json({ error: true, message: "Unauthorized." })
+        .end();
+    }
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ error: true, message: "InternalError" })
+      .end();
+  }
+};
+
+module.exports = loginController;
